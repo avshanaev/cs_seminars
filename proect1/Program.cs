01b26fcc23e2cd42f7da61f382b878470af5b69d
AVShanaev <AVShanaev@gmail.com>
2023-02-27 18:01:26 +0300

    HW 1 seminar

---
+++ proect1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Вводим первое число
+Console.Write("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+// Вводим второе число
+Console.Write("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) Console.WriteLine($"Первое число: {a} больще второго: {b}");
+else if (a < b) Console.WriteLine($"Второе число: {b} больще первого: {a}");
+     else Console.WriteLine($"числа: {a} и: {b} одинаковые");
---
+++ protct2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Вводим первое число
+Console.Write("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+// Вводим второе число
+Console.Write("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+// Вводим третье число
+Console.Write("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+
+if ((a > b) & (a > c)) Console.WriteLine($"Максимум = {a}");
+else if ((b > a) & (b > c)) Console.WriteLine($"Максимум = {b} ");
+     else Console.WriteLine($"Максимум = {c}");
